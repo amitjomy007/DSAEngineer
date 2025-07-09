451b01fda6e3881bcfbad43d306341e4d8b6120e
amitjomy007 <amitjomy007@gmail.com>
2025-07-09 19:45:24 +0530

    feat : time track and remove logic creating exe files repeatedly for each test case

--- compiler/utils/executeUtils/languages/executeCpp.ts
+++ compiler/utils/executeUtils/languages/executeCpp.ts
@@ -10,6 +10,35 @@ if (!fs.existsSync(dirOutput)) {
   fs.mkdirSync(dirOutput, { recursive: true });
 }
 
+const generateExeFile = (codePath: string, outPath: string) => {
+  const commmand = `g++ ${codePath} -o ${outPath}`;
+  const resultPromise = new Promise((resolve, reject) => {
+    exec(commmand, (err: any, stdout: string, stderr: any) => {
+      if (err) {
+        //error appeared in the code written by us
+        reject(err);
+      }
+      if (stderr) {
+        //error appeared in the code written by the client (asdf.cpp for example)
+        //this is not an error in the code written by us
+        //console.error(`C++ compilation stderr: ${stderr}`);
+        reject(new Error(`C++ compilation stderr: ${stderr}`));
+        //return { message: "C++ compilation error", stderr };
+      }
+      console.log(`C++ compilation stdout: ${stdout}`);
+      console.log("----------");
+      resolve(stdout);
+    });
+  });
+  resultPromise
+    .then((result) => {
+      return result;
+    })
+    .catch((error) => {
+      return error;
+    });
+  return resultPromise;
+};
 const runSingleTestCase = (
   inputPath: string,
   outPath: string,
@@ -17,23 +46,27 @@ const runSingleTestCase = (
   outputFileName: string
 ): any => {
   return new Promise((resolve, reject) => {
-    const commmand = `g++ ${codePath} -o ${outPath} && cd ${dirOutput} && ${outputFileName} < ${inputPath}`;
-    exec(commmand, (err: any, stdout: string, stderr: any) => {
-      if (err) {
-        //error appeared in the code written by us
-        reject(err);
-        return;
+    const commmand = `${outputFileName} < ${inputPath}`;
+    exec(
+      commmand,
+      { cwd: dirOutput },
+      (err: any, stdout: string, stderr: any) => {
+        if (err) {
+          //error appeared in the code written by us
+          reject(err);
+          return;
+        }
+        if (stderr) {
+          //error appeared in the code written by the client (asdf.cpp for example)
+          //this is not an error in the code written by us
+          console.error(`C++ execution stderr: ${stderr}`);
+          return { message: "C++ execution error", stderr };
+        }
+        console.log(`C++ execution stdout: ${stdout}`);
+        console.log("----------");
+        resolve(stdout);
       }
-      if (stderr) {
-        //error appeared in the code written by the client (asdf.cpp for example)
-        //this is not an error in the code written by us
-        console.error(`C++ execution stderr: ${stderr}`);
-        return { message: "C++ execution error", stderr };
-      }
-      console.log(`C++ execution stdout: ${stdout}`);
-      console.log("----------");
-      resolve(stdout);
-    });
+    );
   });
 };
 const executeCpp = async (filePath: string, testcaseDir: string) => {
@@ -45,14 +78,24 @@ const executeCpp = async (filePath: string, testcaseDir: string) => {
   const outPath = path.join(dirOutput, `${outputFileName}`);
   try {
     const files = fs.readdirSync(testcaseDir);
+    const compilationTimeStart = Date.now();
+    const compilerMessage = await generateExeFile(filePath, outPath);
+    const compilationTime = Date.now() - compilationTimeStart;
+    console.log("Took time to compile: ", compilationTime);
+    console.log("Compiler said : ", compilerMessage);
+    const start = Date.now();
+
     for (const file of files) {
       const inputPath = path.join(testcaseDir, file);
+      const start = Date.now();
       const output: string = await runSingleTestCase(
         inputPath,
         outPath,
         filePath,
         outputFileName
       );
+      const timeTaken = Date.now() - start;
+      console.log("Took time: ", timeTaken);
       const formattedOutput = output.trim().split(/\r?\n/);
       outputs.push(formattedOutput[0]);
       testcasesExecutedWithinLimits++;
@@ -60,9 +103,11 @@ const executeCpp = async (filePath: string, testcaseDir: string) => {
     }
   } catch (error) {
     console.log("Catched error: ", error);
-    return outputs;
+    const data = { outputs, testcasesExecutedWithinLimits };
+    return data;
   }
-  return outputs;
+  const data = { outputs, testcasesExecutedWithinLimits };
+  return data;
 };
 
 export default executeCpp;
